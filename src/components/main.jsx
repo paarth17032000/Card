f1396b03d2a5eda7c914dc50f76fbe720f9057e8
paarth17032000 <paarth17032000@gmail.com>
2020-12-17 23:37:26 +0530

    text added

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -6,14 +6,29 @@ export default class Main extends Component {
         super(props);
         this.componentRef = React.createRef();
         this.state={
-        
+            val: null
         }
+    }
+    handleSearch = (e) => {
+        this.setState({
+            val: e.target.value
+        })
+        console.log(this.state.val);
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state.val);
     }
     render() {
         return (
             <div>
                 <Drag ref={this.componentRef} />
-                <input className="m-4"/>
+                <form onSubmit={this.handleSubmit}>
+                    <input onChange={this.handleSearch} className="m-4"/>
+                </form>
+                <div style={{backgroundImage: `${this.state.val}`}} className="p-5 border w-50 m-5">
+                    <h6>Welcome to the jungle !!</h6>
+                </div>
             </div>
         )
     }
