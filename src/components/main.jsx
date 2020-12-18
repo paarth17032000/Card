e1f15e1838e93ed5b8b1343e7e7b54bc6643622e
paarth17032000 <paarth17032000@gmail.com>
2020-12-18 23:44:22 +0530

    components made

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -21,15 +21,23 @@ export default class Main extends Component {
     }
     render() {
         return (
-            <div>
-                <Drag ref={this.componentRef} />
-                <form onSubmit={this.handleSubmit}>
-                    <input onChange={this.handleSearch} className="m-4"/>
-                </form>
-                <div style={{backgroundImage: `${this.state.val}`}} className="p-5 border w-50 m-5">
-                    <h6>Welcome to the jungle !!</h6>
+            <div className="container-fluid">
+                <div className="row mx-auto">
+                    <div className="col-xl-4 col-md-5 col-sm-6 bg-success mt-5">
+                        <Drag ref={this.componentRef} />
+                        <form onSubmit={this.handleSubmit}>
+                            <input onChange={this.handleSearch} className="m-4"/>
+                        </form>
+                    </div>
+                    <div className="col-xl-8 col-md-7 col-sm-6 bg-warning mt-5">
+                        <div style={{backgroundImage: `${this.state.val}`}} className="p-5 border w-25 m-5">
+                            <h6>Welcome to the jungle !!</h6>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
     }
 }
+ 
+                
