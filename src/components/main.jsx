2f2f989a13f23a729bed2d0e60d22bd73b13b72a
paarth17032000 <paarth17032000@gmail.com>
2020-12-19 23:17:24 +0530

    components building

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -23,16 +23,21 @@ export default class Main extends Component {
         return (
             <div className="container-fluid">
                 <div className="row mx-auto">
-                    <div className="col-xl-4 col-md-5 col-sm-6 bg-success mt-5">
-                        <Drag ref={this.componentRef} />
-                        <form onSubmit={this.handleSubmit}>
-                            <input onChange={this.handleSearch} className="m-4"/>
-                        </form>
-                    </div>
-                    <div className="col-xl-8 col-md-7 col-sm-6 bg-warning mt-5">
-                        <div style={{backgroundImage: `${this.state.val}`}} className="p-5 border w-25 m-5">
-                            <h6>Welcome to the jungle !!</h6>
+                    <div className="col-xl-4 col-md-5 col-sm-6 border mt-5">
+                        <div className="bg-success my-2 text-center">
+                           <Drag ref={this.componentRef} />
+                            <form onSubmit={this.handleSubmit}>
+                                <input onChange={this.handleSearch} className="m-4"/>
+                            </form> 
                         </div>
+                    </div>
+                    <div className="col-xl-8 col-md-7 col-sm-6 border mt-5">
+                        <div className="bg-warning my-2">
+                            <div style={{backgroundColor: `${this.state.val}`}} className="p-5 border w-75">
+                                <h6>Welcome to the jungle ..</h6>
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
