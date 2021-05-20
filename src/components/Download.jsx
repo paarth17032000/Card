6905d9c036895d62a1bd9ccecd786699f3d3368f
paarth17032000 <paarth17032000@gmail.com>
2021-05-20 16:36:54 +0530

    download corrected

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -8,21 +8,15 @@ export default class Download extends Component {
     }
     render() {
         const {options} = this.props;
-        console.log(this.props.compref)
         return (
             <div>
                 <input
-                type="button"
-                onClick={() => {
-                    options.downloadChoice === "PNG"
-                    ? exportComponentAsPNG(this.props.compref)
-                    : exportComponentAsJPEG(this.props.compref);
-                    {
-                    console.log(exportComponentAsPNG(this.props.compref));
-                    }
-                }}
-                className="mt-3 btn btn-dark"
-                value="Download"
+                    type="button"
+                    onClick={() => {
+                        options.downloadChoice === "PNG" ? exportComponentAsPNG(this.props.compref) : exportComponentAsJPEG(this.props.compref);
+                    }}
+                    className="mt-3 btn btn-dark"
+                    value="Download"
                 />
             </div>
         );
